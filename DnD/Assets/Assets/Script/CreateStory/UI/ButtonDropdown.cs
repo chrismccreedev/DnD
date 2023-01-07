40cc3b162d9c50d4e42067bf0e7389303cbafa68
chrismccreedev <devunity@yahoo.com>
2023-01-07 23:13:41 +0200

    Start Settings

---
+++ DnD/Assets/Assets/Script/CreateStory/UI/ButtonDropdown.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CreateStory
+{
+    public class ButtonDropdown : MonoBehaviour
+    {
+        [SerializeField] private GameObject _buttonPrefab;
+        [SerializeField] private Transform _parents;
+    }
+}
